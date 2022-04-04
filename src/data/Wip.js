72ca8728fd9fe17542c4da75bf52a1139028757e
Chad Parker <63312621+dAppTechie@users.noreply.github.com>
2022-04-03 21:42:59 -0600

    Update

--- src/data/Wip.js
+++ src/data/Wip.js
@@ -1,9 +1,9 @@
 export const WipData = [
   {
     id: 1,
-    type: 'Web App',
+    type: 'Chat App',
     title: 'Hiptzo Chat',
-    desc: 'Web3 chat app I am building to work on blockchain using Moralis and Gun.js for the database, Solidity for smart contracts, Hardhat for testing along with Alchemy. The user will be able to send Ethereum to other users and mint their own NFT as their own avatar.',
+    desc: 'Web3 chat app I am building to work on blockchain using Moralis for the database, Solidity for smart contracts, Hardhat for testing. The user will be able to send Ethereum to other users and mint their own NFT as their own avatar.',
     tags: [
       'Next',
       'TailwindCSS',
@@ -11,12 +11,10 @@ export const WipData = [
       'Daisyui',
       'Ethereum',
       'Solidity',
-      'Gun.js',
       'Moralis',
-      'Alchemy',
       'Hardhat',
     ],
-    visit: 'https://github.com/dAppTechie/Hiptzo-Chat/blob/main/chat.png',
+    visit: 'https://www.hiptzo.chat',
     github: 'https://github.com/dAppTechie/Hiptzo-Chat',
   },
 ];
